83446b6b849c53a62d7bcc25d88cd55c099ffbe3
Ankita <ankyadav1999@gmail.com>
2021-03-28 11:43:23 +0530

    Including Conditional(Menu_Driven) Programs

---
+++ Hotel_Menu.py
@@ -0,0 +1,38 @@
+print("\n......Welcome.....")
+
+while True:
+   
+
+    print("\n Select Your Menu sir: ")
+
+  
+    print("\n1.  Breakfast")
+    print("\n2. Drinks")
+    print("\n3. Dinner")
+    
+      
+    choice = int(input("\n Enter Your Choice: "))
+    if choice == 1:
+        print("\n1.  Poha")
+        print("\n2.  Upma")
+        print("\n3.  Misal")
+        print("\n4.  Bhakricha Chivada")
+        print("\n5.  Fodnicha Bhat")
+
+    elif choice == 2:
+        print("\n1.  Tea")
+        print("\n2.  Coffee")
+        print("\n3.  Milk")
+        print("\n4.  Water")
+        print("\n5.  Cold_Coffee")
+
+    elif choice == 3:
+        print("\n1.  Chikan Tikka")
+        print("\n2.  Pitla Bhakri")
+        print("\n3.  Usal Bhaji")
+        print("\n4.  Masala Rise")
+        print("\n5.  Vang Bhaji")
+
+
+    else:
+        print(" Sorry....Your Choice Is Wrong sir")
